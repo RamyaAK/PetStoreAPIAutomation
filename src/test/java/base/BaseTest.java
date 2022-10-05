9cd7bb33852a4f6be806ec9f49f791eb7ba05ca2
Ramya Sachin <ramyasachin@Ramyas-MacBook-Air.local>
2022-10-05 13:22:37 +0530

    reformatted the code

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,19 @@
 package base;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class BaseTest {
+
+    @BeforeClass
+    public void setUp(){
+        System.out.println("Starting the PetStore!");
+    }
+
+    @AfterClass
+    public void teardown(){
+        System.out.println("PetStore closed.");
+        System.out.println("Thank you for visiting our PetStore!");
+    }
+
 }
--- src/test/java/petStoreAPITests/AddNewPetToStoreTest.java
+++ src/test/java/petStoreAPITests/AddNewPetToStoreTest.java
@@ -3,6 +3,8 @@ package petStoreAPITests;
 import base.BaseTest;
 import builder.CreatePetBuilder;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import petStore.AddNewPetToStore.AddNewPetClient;
 import petStore.AddNewPetToStore.request.AddNewPetRequest;
@@ -11,30 +13,45 @@ import petStore_Entities.Pet;
 
 public class AddNewPetToStoreTest extends BaseTest {
     Pet pet = new Pet();
+    AddNewPetRequest addNewPetRequest;
+    AddNewPetResponse addNewPetResponse;
+
+    @BeforeTest
+    public void prepareTestData() {
+        System.out.println("creating the Pet Object");
+        addNewPetRequest = new CreatePetBuilder().build();
+
+    }
+
     @Test
-    public void AddNewPetToStoreTest(){
-        AddNewPetRequest addNewPetRequest = new CreatePetBuilder().build();
-        AddNewPetResponse addNewPetResponse = new AddNewPetClient().addNewPetToStore(addNewPetRequest);
+    public void AddNewPetToStoreTest() {
 
-        Assert.assertEquals(addNewPetResponse.getName(),"Rocky");
-        Assert.assertEquals(addNewPetResponse.getPhotoUrls().get(0),"in occaecat");
-        String tag1 = addNewPetRequest.getTags().get(0).toString();
-        System.out.println(tag1);
-        Assert.assertEquals(addNewPetResponse.getTags().get(0).get("id"),1412);
-        Assert.assertEquals(addNewPetResponse.getTags().get(1).get("id"),3476);
+        // calling AddNewPetClient with AddNewPetRequest object and saving the result in addNewPetResponse for assertion.
+        addNewPetResponse = new AddNewPetClient().addNewPetToStore(addNewPetRequest);
 
-        Assert.assertEquals(addNewPetResponse.getTags().get(0).get("name"),"tag1");
-        Assert.assertEquals(addNewPetResponse.getTags().get(1).get("name"),"tag2");
+        Assert.assertEquals(addNewPetResponse.getName(), "Rocky");
+        Assert.assertEquals(addNewPetResponse.getPhotoUrls().get(0), "in occaecat");
 
-        Assert.assertEquals(addNewPetResponse.getCategory().getId(),47476709);
-        Assert.assertEquals(addNewPetResponse.getCategory().getName(),"laborum ullamco labore magna");
+        Assert.assertEquals(addNewPetResponse.getTags().get(0).get("id"), 1412);
+        Assert.assertEquals(addNewPetResponse.getTags().get(1).get("id"), 3476);
+
+        Assert.assertEquals(addNewPetResponse.getTags().get(0).get("name"), "tag1");
+        Assert.assertEquals(addNewPetResponse.getTags().get(1).get("name"), "tag2");
+
+        Assert.assertEquals(addNewPetResponse.getCategory().getId(), 47476709);
+        Assert.assertEquals(addNewPetResponse.getCategory().getName(), "laborum ullamco labore magna");
 
         Assert.assertNotNull(addNewPetResponse.getId());
         System.out.println(addNewPetResponse.getId());
 
+        //saving the response petId for future usage.
         pet.setPetId(addNewPetResponse.getId());
         System.out.println(pet.getPetId());
 
+    }
+    @AfterTest
+    public void closeTest(){
+        System.out.println("Test ended successfully!");
     }
 
 }
